aa10187220ff3d176db0518a99820beb6a06c78e
Luke McMurtrie <Lmcmurtrie89@gmail.com>
2021-10-14 15:19:16 -0400

    Service Worker + Manifest link / Budget data serving

--- public/service-worker.js
+++ public/service-worker.js
@@ -2,8 +2,8 @@ const FILES_TO_CACHE = [
     './',
     './index.js',
     './styles.css',
-    './icons/icon-192x192.png',
-    './icons/icon-512x512.png',
+    './icon-192x192.png',
+    './icon-512x512.png',
     `https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css`
 ];
 
