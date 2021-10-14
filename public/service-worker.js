1027c29462d1150bcdec3b3cb8fa5865b6dbcc70
Luke McMurtrie <Lmcmurtrie89@gmail.com>
2021-10-14 14:29:02 -0400

    Manifest.json additions + HTML routing

--- public/service-worker.js
+++ public/service-worker.js
@@ -2,7 +2,9 @@ const FILES_TO_CACHE = [
     './',
     './index.js',
     './styles.css',
-
+    './icons/icon-192x192.png',
+    './icons/icon-512x512.png',
+    `https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css`
 ];
 
 const STATIC_CACHE = "static-cache-v1";
@@ -56,6 +58,4 @@ self.addEventListener("fetch", event => {
           });
         });
       });
-    })
-  );
-});
+    });
